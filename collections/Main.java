b4623e8341732c8cd7bf5f5e57eb3d6633888b2b
Koua Xiong <kxiong517@gmail.com>
2019-08-20 11:30:34 -0500

    created a while loop

--- collections/Main.java
+++ collections/Main.java
@@ -43,12 +43,18 @@ public class Main
 
         System.out.println("*** Array List ***"); // more similar in concept to javascript
 
-        ArrayList<Dogs> dogsArrayList = new ArrayList<Dogs>(); // array list of type Dogs
-        dogsArrayList.addAll(Arrays.asList(dogArray));
+        ArrayList<Dogs> dogsArrayList = new ArrayList<Dogs>(); // created array list of type Dogs with angle brackets, second line is TO INITIALIZE IT
+        dogsArrayList.addAll(Arrays.asList(dogArray)); // will add all of the array from above, first converts to list then takes array and adds it in order
+        dogsArrayList.add(new Dogs("Mutt", 15, true)); // <-- use the method "add" instantiating a new object of type dog, THE WAY to add a single element to bottom of list
 
-        dogsArrayList.add(new Dogs("Mutt", 15, true)); // <-- use the method "add" instantiating a new object of type dog
+        for ( Dogs d : dogsArrayList)
+        {
+            System.out.println(d);
+        }
 
-        for ( Dogs d: dogsArrayList ) // each element is called "d" of type Dogs 
+        System.out.println();
+
+        for ( Dogs d : dogsArrayList ) // each element is called "d" of type Dogs 
         {  
             if (d.getAverageWeight() >= 50)
             {
@@ -65,7 +71,7 @@ public class Main
 
         // cannot compare strings directly with equals ie "koua" == "koua" because strings are objects, 
         // DO NOT COMPARE OBJECTS because will probably not give u results u are after
-
+        // HOW TO COMPARE STRINGS
         if (dogsArrayList.get(2).getBreed().equals("Turtle")) // to compare Strings have to use .equals <- method
         {
             System.out.println("We have a match");
@@ -74,5 +80,31 @@ public class Main
         {
             System.out.println("No match for " + dogsArrayList.get(2).getBreed());
         }
+
+        // arrays and array lists KEEP ORDER
+
+        System.out.println();
+
+        // with an array we knew the length of it beforehand, because we set it up that way
+        // with an arraylist we don't know the "size" of it, so we have to calculate it with a method because it can change over time
+        System.out.println("dogsArrayList is (size) " + dogsArrayList.size()); // calls a method "size" to get the size of array list
+        System.out.println(dogsArrayList.get(3)); // to find a specific element use method "get"
+        dogsArrayList.remove(3); // to remove a specific element use method "remove"
+        System.out.println(dogsArrayList.get(3));
+        System.out.println();
+
+        // in arraylist there are NO EMPTY SPOTS, when we remove it shifts everything up or back in order
+        // iterators come into play when we work with Spring
+        Iterator<Dogs> iterator = dogsArrayList.iterator(); // method inside object arraylist that converts it into an iterator
+        // same result as for loop
+        while (iterator.hasNext()) // hasNext method returns true as long as the iterator still has elements to go like for loop will go thru everything in there
+        {
+            // Dogs tempDog = iterator.next(); // have to call on this method to go to the next element
+            //  System.out.println(tempDog);
+            // *** BOTH WAYS WORK ***
+            System.out.println(iterator.next());
+        }
+
+        System.out.println();
     }
 }
